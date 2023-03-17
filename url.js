1455d7e54d268c70d82a51768808917ab4ab3f4f
9Mmmm <128135165+9Mmmm@users.noreply.github.com>
2023-03-17 17:10:33 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://www.surveycake.com/s/343m9',
+	'https://www.surveycake.com/s/Y6qqZ'
 ];
